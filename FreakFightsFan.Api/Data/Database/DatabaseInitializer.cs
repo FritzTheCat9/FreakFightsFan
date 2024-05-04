7b1c564f11732cb0325199d9307170ad9e234e23
Bartłomiej Umiński <bartlomiejuminski1999@gmail.com>
2024-05-04 11:01:19 +0200

    feat(Database): DatabaseInitializer

--- FreakFightsFan.Api/Data/Database/DatabaseInitializer.cs
+++ FreakFightsFan.Api/Data/Database/DatabaseInitializer.cs
@@ -1,50 +1,36 @@
-﻿using FreakFightsFan.Api.Abstractions;
-using FreakFightsFan.Api.Data.Entities;
-using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.EntityFrameworkCore;
 
 namespace FreakFightsFan.Api.Data.Database
 {
     public class DatabaseInitializer : IHostedService
     {
         private readonly IServiceProvider _serviceProvider;
-        private readonly IClock _clock;
+        private readonly ILogger<DatabaseInitializer> _logger;
 
-        public DatabaseInitializer(IServiceProvider serviceProvider, IClock clock)
+        public DatabaseInitializer(
+            IServiceProvider serviceProvider,
+            ILogger<DatabaseInitializer> logger)
         {
             _serviceProvider = serviceProvider;
-            _clock = clock;
+            _logger = logger;
         }
 
-        public Task StartAsync(CancellationToken cancellationToken)
+        public async Task StartAsync(CancellationToken cancellationToken)
         {
-            using (var scope = _serviceProvider.CreateScope())
+            using var scope = _serviceProvider.CreateScope();
+            var dbContext = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+
+            try
             {
-                //var dbContext = scope.ServiceProvider.GetRequiredService<AppDbContext>();
-                //dbContext.Database.Migrate();
-
-                //var fighters = dbContext.Fighters.ToList();
-
-                //if (fighters.Any())
-                //{
-                //    return Task.CompletedTask;
-                //}
-
-                //fighters =
-                //[
-                //    new() { FirstName = "Marcin", LastName = "Dubiel", Nickname = "Dubiel", Created = _clock.Current(), Modified =  _clock.Current()},
-                //    new() { FirstName = "Kacper", LastName = "Błoński", Nickname = "Crusher", Created = _clock.Current(), Modified =  _clock.Current()},
-                //    new() { FirstName = "Sylwester", LastName = "Wardęga", Nickname = "Zwyrol", Created = _clock.Current(), Modified =  _clock.Current()},
-                //    new() { FirstName = "Michał", LastName = "Baron", Nickname = "Boxdel" , Created = _clock.Current(), Modified =  _clock.Current()},
-                //];
-
-                //dbContext.Fighters.AddRange(fighters);
-                //dbContext.SaveChanges();
+                await dbContext.Database.MigrateAsync(cancellationToken);
             }
-
-            return Task.CompletedTask;
+            catch (Exception ex)
+            {
+                _logger.LogError("[DatabaseInitializer] Error while creating and migrationg the database: {0}", ex.Message);
+            }
         }
 
-        public Task StopAsync(CancellationToken cancellationToken) 
+        public Task StopAsync(CancellationToken cancellationToken)
             => Task.CompletedTask;
     }
 }
