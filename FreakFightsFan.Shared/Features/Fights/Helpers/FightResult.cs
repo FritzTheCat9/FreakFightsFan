90b75f6a767db8b540081a8af911bb655c2e6e1c
Bartłomiej Umiński <bartlomiejuminski1999@gmail.com>
2024-08-17 18:46:10 +0200

    feat: FightResult localization

--- FreakFightsFan.Shared/Features/Fights/Helpers/FightResult.cs
+++ FreakFightsFan.Shared/Features/Fights/Helpers/FightResult.cs
@@ -1,4 +1,7 @@
-﻿namespace FreakFightsFan.Shared.Features.Fights.Helpers
+﻿using System.Globalization;
+using System.Resources;
+
+namespace FreakFightsFan.Shared.Features.Fights.Helpers
 {
     public enum FightResult
     {
@@ -11,15 +14,18 @@
 
     public static class FightResultExtensions
     {
+        private static readonly string _resourceFileNamespace = "FreakFightsFan.Shared.Localization.Resources.FightResultConsts";
+        private static readonly ResourceManager _resourceManager = new(_resourceFileNamespace, typeof(FightResultExtensions).Assembly);
+        
         public static string ToText(this FightResult fightResult)
             => fightResult switch
             {
-                FightResult.UPCOMING => "Upcoming",
-                FightResult.WIN => "Win",
-                FightResult.LOSS => "Loss",
-                FightResult.DRAW => "Draw",
-                FightResult.NO_CONTEST => "No Contest",
-                _ => "Upcoming",
+                FightResult.UPCOMING => _resourceManager.GetString("Upcoming", CultureInfo.CurrentCulture),
+                FightResult.WIN => _resourceManager.GetString("Win", CultureInfo.CurrentCulture),
+                FightResult.LOSS => _resourceManager.GetString("Loss", CultureInfo.CurrentCulture),
+                FightResult.DRAW => _resourceManager.GetString("Draw", CultureInfo.CurrentCulture),
+                FightResult.NO_CONTEST => _resourceManager.GetString("NoContest", CultureInfo.CurrentCulture),
+                _ => _resourceManager.GetString("Upcoming", CultureInfo.CurrentCulture),
             };
     }
 }
